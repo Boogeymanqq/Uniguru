03a94da303eec6eac289ef8b9eb67c4afc1a18ab
Anatoly <anatolykostarev@gmail.com>
2022-04-30 17:17:13 +0300

    Add (#4)
    * Delete style files
    
    * Delete folder
    
    * Delete fonts
    
    * Add fonts
    
    * Add img folder
    
    * Add script
    
    * Add style files
    
    * Create Porfolio component
    
    * Delete images
    
    * Add Portfolio cards
    
    * Fix Header styles
    
    * Fix styles Portfolio section
    
    * Add icon Feedback section
    
    * Link jQuery for Slick Slider
    
    * Create init Slick slider
    
    * Add Slick slider lib
    
    * Link Slick slider styles
    
    * Add cards for Portfolio section
    
    * Add scripts jQuery, Slick slider
    
    * Fix styles
    
    * Fix routes
    
    * Add methos form

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".portfolio__slider").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+  });
+});
