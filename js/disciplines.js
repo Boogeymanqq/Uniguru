92069816d7d0813da96fef4f81e36143f29d9f03
Boogeyman <boogeymanqq@gmail.com>
2022-05-17 23:34:46 +0300

    fix disciplines

---
+++ js/disciplines.js
@@ -0,0 +1,39 @@
+const cardTitle = document.querySelectorAll(".disciplines__card-title");
+const cards = document.querySelectorAll(".disciplines__card");
+const cardList = document.querySelectorAll(".disciplines__list");
+const textBlock = document.querySelectorAll(".disciplines__text");
+const button = document.querySelectorAll(".disciplines-btn");
+
+for (let i = 0; i <= cardTitle.length - 1; i++) {
+  cardTitle[i].addEventListener("click", function (e) {
+    for (let i = 0; i <= cards.length - 1; i++) {
+      const card = cards[i];
+
+      if (e.target === cardTitle[i]) {
+        console.log(e.target);
+        card.classList.toggle("js-disciplines-card");
+      }
+    }
+    for (let i = 0; i <= cardList.length; i++) {
+      const list = cardList[i];
+
+      if (e.target === cardTitle[i]) {
+        list.classList.toggle("js-disciplines-list");
+      }
+    }
+    for (let i = 0; i <= textBlock.length; i++) {
+      const text = textBlock[i];
+      // console.log(button);
+      if (e.target === cardTitle[i]) {
+        text.classList.toggle("js-disciplines-text");
+      }
+    }
+    for (let i = 0; i <= button.length; i++) {
+      const btn = button[i];
+      // console.log(button);
+      if (e.target === cardTitle[i]) {
+        btn.classList.toggle("js-btn");
+      }
+    }
+  });
+}
