e3b93192beabf3cf01898ed9320e68d9dbf9b9d7
Anatoly <anatolykostarev@gmail.com>
2022-05-20 11:29:21 +0300

    Add function to close mobile nav

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -3,6 +3,7 @@
 const $showIcon = document.querySelector(".js-burger");
 const $hideIcon = document.querySelector(".js-mobile-close");
 const $mobileNav = document.querySelector(".js-mobile");
+const $itemNav = Array.from(document.querySelectorAll(".js-items"));
 
 function showNavigation() {
   document.body.style.overflow = "hidden";
@@ -16,3 +17,14 @@ function hideNavigation() {
 
 $showIcon.addEventListener("click", showNavigation);
 $hideIcon.addEventListener("click", hideNavigation);
+
+$itemNav.forEach((elem) => {
+  elem.addEventListener("click", function () {
+    document.body.style.overflow = "";
+    $mobileNav.style.transform = "translateX(100%)";
+  });
+  // return elem.removeEventListener("click", function () {
+  //   document.body.style.overflow = "";
+  //   $mobileNav.style.transform = "translateX(100%)";
+  // });
+});
