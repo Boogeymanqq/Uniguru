e1c1a2404efe9c28971d1c410e295234cdc8ceb1
boogeyman <boogeymanqq@gmail.com>
2022-05-27 02:45:49 +0300

    fix

--- sendmail.php
+++ sendmail.php
@@ -1,34 +1,25 @@
 <?php
    use PHPMailer\PHPMailer\PHPMailer;
-   use PHPMailer\PHPMailer\SMTP;
    use PHPMailer\PHPMailer\Exception;
 
    require '/PHPMailer-6.6.0/src/PHPMailer.php';
-   require '/PHPMailer-6.6.0/src/SMTP';
    require '/PHPMailer-6.6.0/src/Exception.php';
 
    $mail = new PHPMailer(true);
+   $mail->CharSet = 'UTF-8';
 
-   try {
-      $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-      $mail->isSMTP();
-      $mail->Host = 'ssl://smtp.gmail.com';
-      $mail->SMTPAuth   = true;
-      $mail->Username   = 'boogeymanqq@gmail.com';
-      $mail->Password   = 'ozqu qcha lehz avjd';
-      $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-      $mail->Port = 465;
-      $mail->CharSet = "UTF-8";
+   $discipline = $_POST['discipline'];
+   $duration = $_POST['duration'];
+   $country = $_POST['country'];
+   $name = $_POST['name'];
+   $organization = $_POST['organization'];
+   $email = $_POST['email'];
 
-      $mail->setFrom("boogeymanqq@gmail.com", "Письмо от Uniteguru");
-      $mail->addAddress('boogeymanqq@gmail.com');
+   $body = $discipline . ' ' . $duration . ' ' . $country . ' ' . $name . ' ' . $organization . '' . $email;
+   $theme = '[Тема сайта]';
 
-      $mail->isHTML(true);
-      $mail->Subject = 'Here is the subject';
-      $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-      
-      $mail->send();
-   } catch (Exception $e) {
-      echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-  }
-?>
+   $mail->addAddress('boogeymanqq@gmail.com');
+   $mail->Subject = $theme;
+   $mail->Body = $body;
+
+   $mail->send();
