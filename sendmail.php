7c420bcba7c12b2cc621789b08fb2234540b3495
boogeyman <boogeymanqq@gmail.com>
2022-05-27 12:23:03 +0300

    fix

--- sendmail.php
+++ sendmail.php
@@ -2,8 +2,8 @@
    use PHPMailer\PHPMailer\PHPMailer;
    use PHPMailer\PHPMailer\Exception;
 
-   require '/PHPMailer-6.6.0/src/PHPMailer.php';
-   require '/PHPMailer-6.6.0/src/Exception.php';
+   require 'PHPMailer/src/PHPMailer.php';
+   require 'PHPMailer/src/Exception.php';
 
    $mail = new PHPMailer(true);
    $mail->CharSet = 'UTF-8';
@@ -11,13 +11,14 @@
    $discipline = $_POST['discipline'];
    $duration = $_POST['duration'];
    $country = $_POST['country'];
-   $name = $_POST['name'];
+   $name = $_POST['Name'];
    $organization = $_POST['organization'];
    $email = $_POST['email'];
 
-   $body = $discipline . ' ' . $duration . ' ' . $country . ' ' . $name . ' ' . $organization . '' . $email;
+   $body = $discipline . ' ' . $duration . ' ' . $country . ' ' . $name . ' ' . $organization . ' ' . $email;
    $theme = '[Тема сайта]';
 
+   $mail->setFrom('boogeymanqq@gmail.com', 'Mailer');
    $mail->addAddress('boogeymanqq@gmail.com');
    $mail->Subject = $theme;
    $mail->Body = $body;
