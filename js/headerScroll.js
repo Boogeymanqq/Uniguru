70de42711ec53aba970cccbd3e7e82b493206f62
Anatoly <anatolykostarev@gmail.com>
2022-05-30 17:23:42 +0300

    Change scroll method, add show/hide logo by window scroll

--- js/headerScroll.js
+++ js/headerScroll.js
@@ -5,11 +5,22 @@ const $sticky = $header.offsetTop;
 const $headerLogo = document.querySelector(".js-header-logo");
 
 window.addEventListener("scroll", function () {
-  if (window.pageYOffset > $sticky) {
+  if (window.scrollY > $sticky) {
     $header.classList.add("sticky");
-    $headerLogo.style.opacity = 1;
+    if (window.scrollY >= 700) {
+      $headerLogo.style.opacity = 1;
+      $headerLogo.style.transition = "0.2s linear";
+    }
   } else {
     $header.classList.remove("sticky");
     $headerLogo.style.opacity = 0;
   }
+  if (window.scrollY < 700) {
+    $headerLogo.style.opacity = 0;
+    $headerLogo.style.transition = "0.2s linear";
+  }
+});
+
+$headerLogo.addEventListener("click", () => {
+  return window.scroll(0, 0);
 });
